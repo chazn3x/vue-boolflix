465e3c41e4a461791b0343600a3fc8c11da37f8d
Giovanni <gio.a.98@gmail.com>
2021-12-20 16:07:54 +0100

    fine milestone 2

--- src/components/commons/Card.vue
+++ src/components/commons/Card.vue
@@ -2,19 +2,25 @@
     <div class="card">
         <div class="title">
             <span><strong>Titolo: </strong></span>
-            <h3>{{content.title}}</h3>
+            <h3 v-if="content.title != undefined">{{content.title}}</h3>
+            <h3 v-else>{{content.name}}</h3>
         </div>
         <div class="title-orig">
             <span><strong>Titolo originale: </strong></span>
-            <p>{{content.original_title}}</p>
+            <p v-if="content.original_title != undefined">{{content.original_title}}</p>
+            <p v-else>{{content.original_name}}</p>
         </div>
         <div class="lang">
             <span><strong>Lingua: </strong></span>
-            <p>{{content.original_language}}</p>
+            <img v-if="content.langImg != null" :src="content.langImg" :alt="content.title + ' language icon'">
+            <p v-else class="null-img">
+                {{content.original_language}}
+            </p>
         </div>
-        <div v-if="content.vote_average != 0" class="vote">
+        <div class="vote">
             <span><strong>Voto: </strong></span>
-            <p>{{content.vote_average}}</p>
+            <p v-if="content.vote_average != 0">{{content.vote_average}}</p>
+            <p v-else>-</p>
         </div>
     </div>
 </template>
@@ -33,6 +39,21 @@ export default {
     color: white;
     h3, p {
         display: inline;
+    }
+    .lang {
+        img, .null-img {
+            height: 1rem;
+            display: inline-block !important;
+        }
+        .null-img {
+            width: 1rem;
+            height: 1rem;
+            background-color: #555;
+            font-size: .6rem;
+            line-height: 1rem;
+            text-align: center;
+            border-radius: 50%;
+        }
     }
 }
 </style>
--- src/components/commons/Search.vue
+++ src/components/commons/Search.vue
@@ -17,6 +17,8 @@ export default {
     methods: {
         search() {
             if (data.search != '') {
+                data.contents = [];
+                let movies = [], series = [];
                 axios.get('https://api.themoviedb.org/3/search/movie', {
                     params: {
                         api_key: '3390a8a14e621ee87b8e65a286d5c250',
@@ -25,7 +27,26 @@ export default {
                     }
                 })
                 .then(response => {
-                    data.contents = response.data.results;
+                    movies = response.data.results;
+                    axios.get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            api_key: '3390a8a14e621ee87b8e65a286d5c250',
+                            query: data.search,
+                            language: 'it-IT'
+                        }
+                    })
+                    .then(response => {
+                        series = response.data.results;
+                        data.contents = movies.concat(series);
+                        data.contents.forEach(content => {
+                            try {
+                                content.langImg = require('../../assets/img/flags/' + content.original_language + '.png');
+                            }
+                            catch(err) {
+                                content.langImg = null;
+                            }
+                        });
+                    });
                 });
             } else {
                 data.contents = [];
